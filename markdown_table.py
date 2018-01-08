983b2e079007ecb9a117cb6208f2f759b7acc3c1
owatahatena <owatahatena@gmail.com>
2018-01-08 22:49:18 +0900

    'fix'

--- markdown_table.py
+++ markdown_table.py
@@ -1,44 +1,97 @@
 import datetime
 import csv
 
-class Doc:
-    def output_doc(self,doctype):
-        doctype.output_data()
-
 class Markdown:
+    def __init__(self):
+        self.f = open('text.txt','w')
+
+    def output_doc(self,Doc):
+
+        Doc.output_data()
+
+class Doc:
+    def __init__(self,attributes):
+        self.attributes = attributes
+
+    def output_data(self):
+        for attribute in attributes:
+            attribute.output_data()
+
+
+class Table:
     def __init__(self,name):
         self.name = name
+        self.f_in = open(self.name,'r')
+        self.f_out = open('table_out.txt','w')
 
     def output_data(self):
         self.output_head()
-        # self.output_body()
-        # self.output_end()
+        self.output_body()
+        self.file_end()
 
     def data_read(self):
-        dataname = self.name
+        reader = csv.reader(self.f_in)
 
-        with open(dataname,'r') as f:
-            reader = csv.reader(f)
-            header = next(reader)
+        return reader
 
-        return header
+    def file_end(self):
+        self.f_in.close()
+        self.f_out.close()
 
     def output_head(self):
-        header = self.data_read()
-        s_h = " | "
+        reader = self.data_read()
+        header = next(reader)
         l = []
         for head in header:
             l.append("|")
             l.append(head)
-        # l.append("|")
+
+        #print(l)
 
         for i in l:
             print(i,end="")
+        print("\n")
+
+        print(l)
+
+        s = ' '.join(l)
+        #print(s)
+        self.f_out.write(s)
+        self.f_out.write('\n')
+
+
+
+
+    def output_body(self):
+        reader = self.data_read()
+        l = [[]]
+        for row in reader:
+            tmp = []
+            for r in row:
+                tmp.append("|")
+                tmp.append(r)
+            l.append(tmp)
+
+        del l[0]
+
+        # for i in l:
+        #     for j in i:
+        #         print(j,end="")
+        #     print("\n")
+
+        for i in l:
+            print(i)
+            s = ' '.join(i)
+            self.f_out.write(s)
+            self.f_out.write('\n')
+
+
+
 
 
 if __name__ == '__main__':
 
-    doc = Doc()
+    doc = Markdown()
 
     name = "samle.csv"
-    doc.output_doc(Markdown(name))
+    doc.output_doc(Table(name))
