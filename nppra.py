2b04428c60161abcf37f61ca3fcca468edd0694a
owatahatena <owatahatena@gmail.com>
2018-01-17 16:41:24 +0900

    '更に回答'

--- nppra.py
+++ nppra.py
@@ -45,4 +45,35 @@ a = np.eye(3)
 print(a)
 
 #12
-print(random.random((3,3,3))
+print(np.random.random((3,3,3))
+
+
+#13
+a = np.random.random((3,3,3))
+minn = a.min()
+maxx = a.max()
+print(minn,maxx)
+
+#14
+a = np.random.random(30)
+print(a.mean())
+
+#15 see ans
+Z = np.ones((10,10))
+Z[1:-1,1:-1] = 0
+print(Z)
+
+#16 see ans pad == padding
+Z = np.ones((5,5))
+Z = np.pad(Z, pad_width=1, mode='constant', constant_values=0)
+print(Z)
+
+#17 see ans  diag==対角成分の取得
+Z = np.diag(1+np.arange(4),k=-1)
+print(Z)
+
+#18
+Z = np.zeros((8,8),dtype=int)
+Z[1::2,::2] = 1
+Z[::2,1::2] = 1
+print(Z)
